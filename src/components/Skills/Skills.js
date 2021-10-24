76f1e72add78d4439c49fde4c347b344c03dd736
Corey Miller <coreymiller454@gmail.com>
2021-10-24 00:55:59 -0400

    add logos to skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,10 @@
 import React from "react";
+import nodeLogoSmall from "../../img/logos/node-small.png";
+import reactLogoSmall from "../../img/logos/react-small.png";
+import mongoLogoSmall from "../../img/logos/mongodb-icon-small.png";
+import cssLogoSmall from "../../img/logos/css3.png";
+import htmlLogoSmall from "../../img/logos/html5.png";
+import sassLogo from "../../img/logos/sassLogo.png";
 
 const Skills = () => {
 	return (
@@ -11,15 +17,25 @@ const Skills = () => {
 				<span className="title__letter">L</span>
 				<span className="title__letter">S</span>
 			</div>
-			<div>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam eius
-					laboriosam soluta! At dolor omnis aut ducimus id ea qui, autem
-					aspernatur? Ad magni illo corporis. Veniam recusandae sunt illum,
-					repellendus blanditiis repellat labore aspernatur unde et, libero
-					facere fuga a quibusdam? Dignissimos quod quis dolorum aliquam?
-					Itaque, dicta ad.
-				</p>
+			<div className="Skills__logos">
+				<div className="wrapper">
+					<img src={reactLogoSmall} className="logo" alt="react logo" />
+				</div>
+				<div className="wrapper">
+					<img src={nodeLogoSmall} className="logo" alt="node.js logo" />
+				</div>
+				<div className="wrapper">
+					<img src={mongoLogoSmall} className="logo" alt="MongoDB logo" />
+				</div>
+				<div className="wrapper">
+					<img src={htmlLogoSmall} className="logo" alt="HTML5 logo" />
+				</div>
+				<div className="wrapper">
+					<img src={cssLogoSmall} className="logo" alt="CSS3 logo" />
+				</div>
+				<div className="wrapper">
+					<img src={sassLogo} className="logo" alt="Sass logo" />
+				</div>
 			</div>
 		</div>
 	);
